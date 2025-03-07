71f92cbf0a5b82f477a40f05f23d32c3798e3e7a
Vladislav Stasenko <100155839+vladislabv@users.noreply.github.com>
2025-03-07 18:29:06 +0100

    Develop to Staging (#10)
    * Connect UI with backend (#9)
    
    * Add backend integration
    
    * Change calling env variable to satisfy pytest

--- app/schemas.py
+++ app/schemas.py
@@ -50,10 +50,37 @@ def get_features() -> dict:
             "options": ["Husband", "Wife"],
             "help": "Select your relationship.",
         },
-        # "race": {"type": "select", "options": ["Black", "White"], "help": "Select your race."},
+        "race": {
+            "type": "select",
+            "options": ["Black", "White"],
+            "help": "Select your race.",
+        },
         "gender": {
             "type": "select",
             "options": ["Male", "Female"],
             "help": "Select your gender.",
+        },
+        "capital_gain": {
+            "type": "number",
+            "min_value": 0,
+            "max_value": 10_000_000_000,
+            "help": "Insert your capital gain.",
+        },
+        "capital_loss": {
+            "type": "number",
+            "min_value": 0,
+            "max_value": 10_000_000_000,
+            "help": "Insert your capital loss.",
+        },
+        "hours_per_week": {
+            "type": "number",
+            "min_value": 0,
+            "max_value": 65,
+            "help": "Insert your hours per week.",
+        },
+        "native_country": {
+            "type": "select",
+            "options": ["USA", "Germany", "France", "Spain"],
+            "help": "Insert your country of origin.",
         },
     }
